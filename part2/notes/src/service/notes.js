4acbc481701e048414fe3fccdb02e0099299cb32
gonzalo00913 <gonzalo-masa@outlook.com>
2023-09-23 10:20:42 -0300

    ✔ Build app

--- part2/notes/src/service/notes.js
+++ part2/notes/src/service/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "/api/notes";
+const baseUrl = "https://back-end-notes-nuevo.onrender.com/api/notes";
 
 const getAll = () => {
   return axios.get(baseUrl);
