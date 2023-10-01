3c113e0945bb033d2aa25260b74109379bf8b0d0
gonzalo00913 <gonzalo-masa@outlook.com>
2023-10-01 13:14:43 -0300

    ✅Part3 testing con jest

--- part3/notes/models/note.js
+++ part3/notes/models/note.js
@@ -1,17 +1,17 @@
-require("dotenv").config()
-const mongoose = require("mongoose")
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minlength: 5,
-      required: true
-    },
-    date: { 
-      type: Date,
-      required: true
-    },
-    important: Boolean
-  })
-  
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
+})
+
 module.exports = mongoose.model('Note', noteSchema)
