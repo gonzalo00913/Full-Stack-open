6d1e31f4774b7ca15eb702958333632400fab0bd
gonzalo00913 <gonzalo-masa@outlook.com>
2023-11-07 09:45:51 -0300

    redux toolkit

--- part6/notes-redux/src/components/VisibilityFilter.jsx
+++ part6/notes-redux/src/components/VisibilityFilter.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { filterChange } from '../reducers/filterReducer'
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from '../reducers/filterReducer';
 
-const VisibilityFilter = (props) => {
-  const dispatch = useDispatch()
+const VisibilityFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector((state) => state.filter);
+
+  const handleFilterChange = (newFilter) => {
+    dispatch(setFilter(newFilter));
+  };
 
   return (
     <div>
-      all    
-      <input 
-        type="radio" 
-        name="filter" 
-        onChange={() => dispatch(filterChange('ALL'))}
-      />
-      important   
-      <input
-        type="radio"
-        name="filter"
-        onChange={() => dispatch(filterChange('IMPORTANT'))}
-      />
-      nonimportant 
-      <input
-        type="radio"
-        name="filter"
-        onChange={() => dispatch(filterChange('NONIMPORTANT'))}
-      />
-    </div>
-  )
-}
+      <label>
+        <input
+          type="radio"
+          name="filter"
+          value="ALL"
+          checked={filter === 'ALL'}
+          onChange={() => handleFilterChange('ALL')}
+        />
+        All
+      </label>
 
-export default VisibilityFilter
+      <label>
+        <input
+          type="radio"
+          name="filter"
+          value="IMPORTANT"
+          checked={filter === 'IMPORTANT'}
+          onChange={() => handleFilterChange('IMPORTANT')}
+        />
+        Important
+      </label>
+
+      <label>
+        <input
+          type="radio"
+          name="filter"
+          value="NONIMPORTANT"
+          checked={filter === 'NONIMPORTANT'}
+          onChange={() => handleFilterChange('NONIMPORTANT')}
+        />
+        Non-Important
+      </label>
+    </div>
+  );
+};
+
+export default VisibilityFilter;
--- part6/notes-redux/src/reducers/filterReducer.js
+++ part6/notes-redux/src/reducers/filterReducer.js
@@ -1,16 +1,15 @@
-const filterReducer = (state = 'ALL', action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.filter
-      default:
-        return state
-    }
-  }
-  export const filterChange = filter => {
-    return {
-      type: 'SET_FILTER',
-      filter,
-    }
-  }
-  
-export default filterReducer
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: 'ALL',
+  reducers: {
+    setFilter: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
+
