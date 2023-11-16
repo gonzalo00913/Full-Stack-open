364ad086380319836b4bf0ed35a5b60c618f3e7f
gonzalo00913 <gonzalo-masa@outlook.com>
2023-11-16 18:14:15 -0300

    Sincronización de datos React Query

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -7,15 +7,15 @@ const getAll = async () => {
   return response.data;
 };
 
-const createNew = async (content) =>{
-    const object = {content, votes: 0}
-    const response = await axios.post(baseUrl, object)
-    return response.data
-}
+const createNew = async (content) => {
+  const object = { content, votes: 0 };
+  const response = await axios.post(baseUrl, object);
+  return response.data;
+};
 
-const voteAnecdote = async (id, votes) =>{
-  const response = await axios.patch(`${baseUrl}/${id}`, {votes})
-  return response.data
-}
+const voteAnecdote = async (id, votes) => {
+  const response = await axios.patch(`${baseUrl}/${id}`, { votes });
+  return response.data;
+};
 
 export default { getAll, createNew, voteAnecdote };
