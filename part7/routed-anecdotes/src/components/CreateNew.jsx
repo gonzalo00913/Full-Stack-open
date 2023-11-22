ef2dfe870f773ef8c0bb85e16cfe6e44653f9683
gonzalo00913 <gonzalo-masa@outlook.com>
2023-11-21 23:17:42 -0300

    Custom hooks

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,63 +1,71 @@
 import { useState } from "react";
+import { useField } from "../hooks";
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState("");
-    const [author, setAuthor] = useState("");
-    const [info, setInfo] = useState("");
-    const [notification, setNotification] = useState("");
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      props.addNew({
-        content,
-        author,
-        info,
-        votes: 0,
-      });
-      showNotification(`New anecdote created ${content}`)
-    };
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+  const [notification, setNotification] = useState("");
 
-    const showNotification = (message) => {
-        setNotification(message);
-        setTimeout(() => {
-          setNotification(null);
-        }, 5000);
-      };
-      
-  
-    return (
-      <div>
-        {notification}
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            content
-            <input
-              name="content"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
-          </div>
-          <div>
-            author
-            <input
-              name="author"
-              value={author}
-              onChange={(e) => setAuthor(e.target.value)}
-            />
-          </div>
-          <div>
-            url for more info
-            <input
-              name="info"
-              value={info}
-              onChange={(e) => setInfo(e.target.value)}
-            />
-          </div>
-          <button>create</button>
-        </form>
-      </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    });
+    content.onReset();
+    author.onReset();
+    info.onReset();
+    showNotification(`New anecdote created ${content.value}`);
   };
+
+  const handleReset = () => {
+    content.onReset();
+    author.onReset();
+    info.onReset();
+  };
+
+  const showNotification = (message) => {
+    setNotification(message);
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  };
+
+  return (
+    <div>
+      {notification}
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
+          />
+        </div>
+        <div>
+          url for more info
+          <input type={info.type} value={info.value} onChange={info.onChange} />
+        </div>
+        <button>create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default CreateNew;
